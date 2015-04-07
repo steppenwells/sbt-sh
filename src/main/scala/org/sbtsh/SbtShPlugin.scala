df1fe2607e878e53708f28d6f07e223610fae95d
Cary M. Robbins <carymrobbins@gmail.com>
2015-04-07 10:05:13 -0500

    Updated to build with sbt-0.13; refs #5

--- src/main/scala/org/sbtsh/SbtShPlugin.scala
+++ src/main/scala/org/sbtsh/SbtShPlugin.scala
@@ -1,15 +1,17 @@
 package org.sbtsh
 
 import sbt._
-import sbt.CommandSupport._
+
+import scala.language.postfixOps
 
 object SbtShPlugin extends Plugin {
 
   override lazy val settings = Seq(Keys.commands += shCommand)
 
-  def shCommand = Command.args("sh", "<shell command>") { (state, args) => 
-    val ret = args.mkString(" ") !
-    
+  def shCommand = Command.args("sh", "<shell command>") { (state, args) =>
+    // Execute the shell command.
+    args.mkString(" ") !
+
     state
   }
 
